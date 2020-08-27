59353b5edcae8c1c2ec66d0fdccb5c479c1651a2
Ubuntu <usridzero@gmail.com>
2020-08-27 14:54:48 +0000

    Lesson-02.

---
+++ lesson-2/script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+## This is a test
+#def age(a):
+age=int(input('How old are you?: '))
+#age=int(input())
+if age < int(18):
+	print("You are under 18.")
+elif (age == int(18)):
+	print("You are 18.")
+else:
+	print("You are above 18.")
+
+
